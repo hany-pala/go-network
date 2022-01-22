9910602d8279173cc820752b64c66cd267cd7606
HANE <30589585+inspiration33@users.noreply.github.com>
2022-01-22 16:22:03 +0900

    fix: annotation

--- listen_test.go
+++ listen_test.go
@@ -1,6 +1,6 @@
 package ch03
 
-import(
+import (
 	"net"
 	"testing"
 )
@@ -10,21 +10,21 @@ func TestListener(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	
-	defer func () { _ = listener.Close() }()
-	
+
+	defer func() { _ = listener.Close() }()
+
 	t.Logf("bound to %q", listener.Addr())
-	
+
 	// 하나 이상의 수신 연결을 처리하기 위해 for loop
 	for {
 		//wait to connect
-		//Accept() - 클라-서버간의 tcp 핸드셰이크 절차가 완료될때까지 블로킹됨. 
+		//Accept() - 클라-서버간의 tcp 핸드셰이크 절차가 완료될때까지 블로킹됨.
 		conn, err := listener.Accept()
 		if err != nil {
 			return err
 		}
-		
-		go func (c net.Conn) {
+
+		go func(c net.Conn) {
 			// 서버로 FIN 패킷 보냄
 			defer c.Close()
 		}(conn)
